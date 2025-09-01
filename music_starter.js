5808a39bc379824bf6677993152c4e701eb4a456
alyssawyndham <alyssawyndham@gmail.com>
2025-09-02 11:45:21 +1200

    workin

--- music_starter.js
+++ music_starter.js
@@ -2,7 +2,7 @@
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   background(20)
-  textFont('Verdana'); // please use CSS safe fonts
+  textFont('Helvetica'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
   
